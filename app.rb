e6e6d9076fb6b860965e4dbc457aab44c10206b4
Grant Austin <gaustin@decisiv.com>
2013-11-19 14:01:45 -0700

    add a silly health check

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra/base'
+require 'json'
 
 class App < Sinatra::Base
   get '/' do
     "Hello #{ENV.fetch('APP_ENV') { "Deployment"}}!"
+  end
+
+  get '/health.:type' do |type|
+    @healthy = rand > 0.5 ? true : false
+    case type
+    when 'json'
+      content_type :json
+      { :healthy => @healthy }.to_json
+    else
+      "Health: #{@healthy}"
+    end
   end
 end
